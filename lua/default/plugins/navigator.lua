c01e777d2160e13620df18ccc9e8abc3552d501a
Loenrado <leo.martins02@icloud.com>
2024-01-15 12:31:33 -0500

    Fixed navigator.

--- lua/default/plugins/navigator.lua
+++ lua/default/plugins/navigator.lua
@@ -1,18 +1,20 @@
 return {
 	"alexghergh/nvim-tmux-navigation",
-	lazy = false,
 	cmd = {
-		"TmuxNavigateLeft",
-		"TmuxNavigateDown",
-		"TmuxNavigateUp",
-		"TmuxNavigateRight",
-		"TmuxNavigatePrevious",
+		"NvimTmuxNavigateLeft",
+		"NvimTmuxNavigateDown",
+		"NvimTmuxNavigateUp",
+		"NvimTmuxNavigateRight",
+		"NvimTmuxNavigatePrevious",
 	},
 	keys = {
-		{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-		{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-		{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-		{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-		{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+		{ "<c-h>", "<cmd>NvimTmuxNavigateLeft<cr>" },
+		{ "<c-j>", "<cmd>NvimTmuxNavigateDown<cr>" },
+		{ "<c-k>", "<cmd>NvimTmuxNavigateUp<cr>" },
+		{ "<c-l>", "<cmd>NvimTmuxNavigateRight<cr>" },
+		{ "<c-\\>", "<cmd>NvimTmuxNavigatePrevious<cr>" },
 	},
+	config = function(_, opts)
+		require("nvim-tmux-navigation").setup(opts)
+	end,
 }
